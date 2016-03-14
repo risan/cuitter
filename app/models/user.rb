41b983707f71a56831e0368028a0cc0d19c40b25
risan <risanbagja@gmail.com>
2016-03-14 23:46:01 +0100

    Add user fixture and test for valid login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   before_save { self.email.downcase! }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:foo)
+  end
+
   test "invalid login data" do
     get login_path
     assert_template "sessions/new"
@@ -10,5 +14,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test "valid login data" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, session: { email: @user.email, password: "secret" }
+    assert_redirected_to @user
+    follow_redirect!
+
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
