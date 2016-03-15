39da9c0ce5e05a0c183a5e6eeec00d0e6008cd4e
risan <risanbagja@gmail.com>
2016-03-15 20:42:18 +0100

    Complete update profile feature.

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:foo)
+  end
+
+  test "invalid edit data" do
+    get edit_user_path @user
+    assert_template "users/edit"
+    patch user_path(@user), user: { name: "", email: "foo", password: "bar", password_confirmation: "baz" }
+    assert_template "users/edit"
+    assert_select "div.error-explanation"
+    assert_select "div.field_with_errors"
+  end
+
+  test "valid edit data" do
+    get edit_user_path @user
+    assert_template "users/edit"
+
+    new_name = "John Doe"
+    new_email = "john@doe.com"
+    patch user_path(@user), user: { name: new_name, email: new_email, password: "", password_confirmation: "" }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+
+    @user.reload
+    assert_equal new_name, @user.name
+    assert_equal new_email, @user.email
+  end
+end
